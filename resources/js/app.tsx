5db3b94c04821f366851927dd92bca275ef133ff
Luis Güette <guette@kirschbaumdevelopment.com>
2022-05-21 18:47:13 -0400

    Initial commit

---
+++ resources/js/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+
+createInertiaApp({
+  // eslint-disable-next-line import/no-dynamic-require
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    render(<App {...props} />, el);
+  },
+});
